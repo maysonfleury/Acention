7b515eda37f64c150b592029bc1ed2f63852d234
Mayson <mayson11@live.ca>
2021-10-17 15:06:21 -0400

    We have basic movement babyyyy

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 300f;
 
     public Transform playerBody;
 
@@ -19,13 +19,26 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Getting player input
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        // Stops the camera from going insane on Start
+        if(Mathf.Abs(mouseY) > 100)
+        {
+            mouseY = 0;
+        }
+        if(Mathf.Abs(mouseX) > 100)
+        {
+            mouseX = 0;
+        }
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);  // Limits the vertical look
+
+        // Looking up/down rotates the camera
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        // Looking left/right rotates the player model
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
