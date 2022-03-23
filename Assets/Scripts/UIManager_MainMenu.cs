67e22fc2a67b9781883c281431c1708599d7fd1b
Mayson <mayson11@live.ca>
2022-03-23 19:31:09 -0400

    Main menu in progress

---
+++ Assets/Scripts/UIManager_MainMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class UIManager_MainMenu : MonoBehaviour
+{
+    [SerializeField] Image titleImage;
+
+    private float m_VolumeRef = 1f;
+    private bool m_Paused;
+
+    [SerializeField] GameObject controlMenu;
+    [SerializeField] GameObject settingsMenu;
+    [SerializeField] GameObject returnButton;
+
+    [SerializeField] TextMeshProUGUI mouseSens;
+
+    MusicManager mm;
+
+    private void Awake()
+    {
+        mm = FindObjectOfType<MusicManager>();
+    }
+
+    private void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * 0.3f);
+    }
+
+    void NewGame()
+    {
+        // Delete Old Save
+        // Switch to main game scene
+    }
+
+    void ContinueGame()
+    {
+        // Check for Save file
+        // Continue from save file if there is one, new game if not
+    }
+
+    void QuitGame()
+    {
+        // TODO: Save?
+        Application.Quit();
+    }
+}
