69d115fd8dbb79913f94b3fd0c6b1e995347e2cf
Mayson <mayson11@live.ca>
2023-08-03 19:39:18 -0400

    Importing new unity input + camera systems
    Also Acention New scene

---
+++ Assets/ReticleController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticleController : MonoBehaviour
+{
+    [SerializeField] private enum ReticleType
+    {
+        
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Enum
+        // Change depending on player script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
